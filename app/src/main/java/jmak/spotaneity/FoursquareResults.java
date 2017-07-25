58f667cae4904c7915d67b98e5e0828abc95dc99
Ryanluu2017 <ryanluu12345@gmail.com>
2017-07-25 01:34:06 -0700

    Added Foursquare Java API. However, experiencing problems with null object pointer references, possibly indicating a faulty connection to the API or a misdone API connection.

--- app/src/main/java/jmak/spotaneity/FoursquareResults.java
+++ app/src/main/java/jmak/spotaneity/FoursquareResults.java
@@ -1,10 +1,22 @@
 package jmak.spotaneity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import fi.foyt.foursquare.api.FoursquareApi;
+import fi.foyt.foursquare.api.FoursquareApiException;
+import fi.foyt.foursquare.api.Result;
+import fi.foyt.foursquare.api.entities.CompactVenue;
+import fi.foyt.foursquare.api.entities.VenueGroup;
+import fi.foyt.foursquare.api.entities.VenuesSearchResult;
 
 /**
  * Created by Ryanluu2017 on 7/23/2017.
@@ -25,6 +37,74 @@ public class FoursquareResults extends AppCompatActivity {
         //Sets the text of the text view equal to a custom text
         TextView fourSquarePage=(TextView) findViewById(R.id.fourSquareText);
         fourSquarePage.setText("spot events");
+
+       new getFourSquareAPI().execute();
+
+
+    }
+
+
+    protected class getFourSquareAPI extends AsyncTask<Void, Void, ArrayList<String>> {
+
+        protected ArrayList<String> placeNames=new ArrayList<String>();
+        protected ArrayList<String> gah=new ArrayList<String>();
+
+
+
+        @Override
+        protected ArrayList<String> doInBackground(Void...params){
+            //Initializes FoursquareAPI instance
+            //Gathers data about a venue and then adds stuff to a list
+            gah.add("bah"); //Take out later
+            //Attempts to connect to the FourSquare API and extract data from the api
+            try {
+                FoursquareApi apiCall = new FoursquareApi("VPPJ5HFUV3QJCPA2NZ4ZR34TJWJBYLPVBRMRR4MIZIIKRCZJ", "L5VM5RZYC3Z3HW4FWBTAHAO3UQ25Z2HPO2ZA3AUL2522Z3LZ","https://spotaneity.com");
+
+
+
+                    placeNames.add(apiCall.toString());
+
+                Result<VenuesSearchResult> result = apiCall.venuesSearch("coffee", null, null, null, null, null, null, null, null, null, null);
+
+                placeNames.add(result.getResult().toString());
+                // if query was ok we can finally we do something with the data
+              /*      for (CompactVenue venue : result.getResult().getVenues()) {
+                        // TODO: Do something with the data
+                        placeNames.add(venue.getName());
+                    }
+*/
+            }
+
+
+
+
+            catch(FoursquareApiException e){
+                e.printStackTrace();
+                Intent myIntent=new Intent(FoursquareResults.this,HomePage.class);
+                startActivity(myIntent);
+
+            }
+
+
+
+            return placeNames;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<String> venues){
+
+            TextView randView=(TextView) findViewById(R.id.fourSquareText);
+            randView.setText(venues.get(0));
+
+            ListView spotViews=(ListView) findViewById(R.id.spotView);
+            ArrayAdapter venueAdapter=new ArrayAdapter(FoursquareResults.this,android.R.layout.simple_list_item_1,venues);
+            spotViews.setAdapter(venueAdapter);
+
+
+        }
+
+
+
 
     }
 
