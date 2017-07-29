9ec19da7e0c2559f76f1ae6bf1491474bcbda0ba
Ryanluu2017 <ryanluu12345@gmail.com>
2017-07-28 17:23:39 -0700

    Added in Google Places functionality. The GooglePlacePicker activitiy allows a user to pick a spot on the map and extract relevant details about the location. Further plans include: making items selectable and linking items to corresponding actions. For example, linking an address to Google direction services.

--- app/src/main/java/jmak/spotaneity/GooglePlacePicker.java
+++ app/src/main/java/jmak/spotaneity/GooglePlacePicker.java
@@ -2,28 +2,112 @@ package jmak.spotaneity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.FragmentActivity;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.view.View;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+import java.util.ArrayList;
+
 
 /**
  * Created by Ryanluu2017 on 7/27/2017.
  */
 
-public class GooglePlacePicker extends FragmentActivity {
+public class GooglePlacePicker extends AppCompatActivity {
+
+    //Widget variables
+    private ListView placeView;
+    private Button placeButton;
+
+    //Place picker variables
+    private int PLACE_PICKER_REQUEST=1; //1 for release //2 for debug
+
+    //Organizing info variables
+    private ArrayList<String> placeList=new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+            setContentView(R.layout.map_page);
+
+            placeButton=(Button) findViewById(R.id.placeButton);
+            placeButton.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View v){
+                    PlacePicker.IntentBuilder builder=new PlacePicker.IntentBuilder();
+
+                    Intent placeIntent;
+
+                    try {
+
+
+                        placeIntent = builder.build(GooglePlacePicker.this);
+                        startActivityForResult(placeIntent,PLACE_PICKER_REQUEST);
+                    }
+                    catch(Exception e){
+
+
+                    }
+
+                    }
+
+            });
 
 
 
         };
+
+        protected void onActivityResult(int requestCode,int resultCode,Intent data){
+
+            if (requestCode==PLACE_PICKER_REQUEST){
+
+                if (resultCode==RESULT_OK){
+
+                    //Gets the user's pick of the place from the PlacePicker UI
+                    Place place=PlacePicker.getPlace(getApplicationContext(),data);
+
+                    //Gets attributes from the place
+                    String name=String.format("Name: %s",place.getName().toString());
+                    String address=String.format("Place: %s",place.getAddress().toString());
+
+                    placeList.add(name);
+                    placeList.add(address);
+                    if (place.getWebsiteUri()!=null) {
+                        String site = String.format("Website Link: %s", place.getWebsiteUri().toString());
+                        placeList.add(site);
+                    }
+
+
+                    //Calls a function that sets the list adapter for these items
+                    setListAdapter();
+
+
+
+
+                }
+
+            }
+
+        }
+
+        protected void setListAdapter(){
+
+            //Creates an adapter for the info data form the PlacePicker before
+            ArrayAdapter infoAdapter=new ArrayAdapter(getApplicationContext(),android.R.layout.simple_list_item_1,placeList);
+
+            //Sets up the list view and then sets the adapter to the one created above
+            placeView=(ListView) findViewById(R.id.placesListView);
+            placeView.setAdapter(infoAdapter);
+
+        }
+
 
 
 }
